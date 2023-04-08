73c497843bf24bcbf2bb91a8f5b0660dbb7dfc38
teodor <teodorkostadinov@abv.bg>
2023-04-08 14:06:03 +0300

    Testing Nested Loops.

---
+++ Week_6/Day_1/sum_of_two_numbers.py
@@ -0,0 +1,23 @@
+start_interval = int(input())
+final_interval = int(input())
+magic_number = int(input())
+
+combination_counter = 0
+flag = False
+
+for x in range(start_interval , final_interval + 1):
+    for y in range(start_interval, final_interval + 1):
+
+        combination_counter += 1
+
+        if x + y == magic_number:
+            flag = True
+            break
+
+    if flag:
+        break
+
+if flag:
+    print(f"Combination N:{combination_counter} ({x} + {y} = {magic_number})")
+else:
+    print(f"{combination_counter} combinations - neither equals {magic_number}")
